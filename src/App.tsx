2e2e61819a562656209e566cb25eb94edf9149a0
haight <gao_ht@126.com>
2020-05-23 15:16:43 +0800

    7-1 图标解决方案简介

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Menu from './components/menu/menu'
 import MenuItem from './components/menu/menuItem'
 import SubMenu from './components/menu/subMenu'
@@ -6,6 +8,7 @@ import SubMenu from './components/menu/subMenu'
 function App() {
   return (
     <div className='App'>
+      <FontAwesomeIcon icon={faCoffee} size='10x' />
       <header className='App-header'>
         <Menu
           mode='vertical'
